32731a0e3fa2dbd20ac4d53c5f9aaf0fd3184e3f
KIBA <kjh030529@gmail.com>
2022-07-21 10:49:31 +0900

    Initialize Repo

---
+++ CSharp/LGT/Heap.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heap
+{
+    public interface IComparable
+    {
+        public static bool operator >(IComparable a, IComparable b) => a > b;
+        public static bool operator <(IComparable a, IComparable b) => a < b;
+    }
+
+    public class CustomHeap<T> where T : IComparable
+    {
+        public CustomHeap()
+        { }
+
+        public List<IComparable> heap = new List<IComparable>(); //Use List<> instead of DynamicArray
+
+        public void Add(IComparable data)
+        {
+            heap.Add(data);
+
+            var lastIndex = heap.Count - 1;
+            OrderFromEnd(lastIndex);
+        }
+
+        public IComparable Remove(int index)
+        {
+            var removedObject = heap[index];
+
+            var lastIndex = heap.Count - 1;
+            heap[index] = heap[lastIndex];
+            heap.RemoveAt(lastIndex);
+
+            OrderFromFirst(index);
+
+            return removedObject;
+        }
+
+        public void OrderFromEnd(int lastIndex)
+        {
+            var parentIndex = lastIndex / 2;
+            if (heap[lastIndex] > heap[parentIndex])
+            {
+                var tmp = heap[parentIndex];
+                heap[parentIndex] = heap[lastIndex];
+                heap[lastIndex] = tmp;
+                OrderFromEnd(parentIndex);
+            }
+        }
+
+        public void OrderFromFirst(int parentIndex)
+        {
+            var leftChildIndex = parentIndex / 2;
+            var rightChildIndex = leftChildIndex + 1;
+
+            var largestIndex = -1;
+            if (heap[leftChildIndex] > heap[rightChildIndex])
+            {
+                if (heap[leftChildIndex] < heap[parentIndex]) return;
+                largestIndex = leftChildIndex;
+            }
+            else
+            {
+                if (heap[rightChildIndex] < heap[parentIndex]) return;
+                largestIndex = rightChildIndex;
+            }
+
+            var tmp = heap[largestIndex];
+            heap[largestIndex] = heap[parentIndex];
+            heap[parentIndex] = tmp;
+            OrderFromFirst(largestIndex);
+        }
+    }
+}
---
+++ CSharp/LGT/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Heap;
+
+namespace LGT
+{
+    class Program
+    {
+        public class Machine : Heap.IComparable
+        {
+            public int ID;
+            public float Avail = 0;
+        }
+
+        public CustomHeap<Machine> Heap = new CustomHeap<Machine>();
+
+        static void Main(string[] args)
+        {
+            for(var i = 0; i< 3; i++) // 3 is machine count
+            {
+
+            }
+        }
+    }
+}
